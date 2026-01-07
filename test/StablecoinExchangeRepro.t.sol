1f50f21e0de79be462747b9a84e5c9f417729f35
grandizzy <grandizzy.the.egg@gmail.com>
2026-01-07 10:48:24 +0200

    Update

--- test/StablecoinExchangeRepro.t.sol
+++ test/StablecoinExchangeRepro.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {IStablecoinExchange} from "tempo-std/interfaces/IStablecoinExchange.sol";
 import {ITIP20} from "tempo-std/interfaces/ITIP20.sol";
 
@@ -16,10 +16,17 @@ contract StablecoinExchangeReproTest is Test {
         vm.startPrank(0xBfaA9CEFb6c7d4537EceE5d036341BF4FACbf20e);
         betaUsd.approve(address(exchange), 10000000000000);
         pathUsd.approve(address(exchange), 10000000000000);
-        
+
         uint256 initialExchangeBalance = pathUsd.balanceOf(address(exchange));
         exchange.place(address(betaUsd), 9900000011, true, 10);
-        assertEq(initialExchangeBalance + 9900000011, pathUsd.balanceOf(address(exchange)), "pathUSD exchange balance different than expected");
+
+        uint32 price = exchange.tickToPrice(10);
+        uint256 expectedEscrow = (uint256(9900000011) * uint256(price)) / uint256(exchange.PRICE_SCALE());
+
+        console2.log("expectedEscrow", expectedEscrow);
+        console2.log("expectedBalance", initialExchangeBalance + expectedEscrow);
+        console2.log("currentBalance", pathUsd.balanceOf(address(exchange)));
+        assertEq(initialExchangeBalance + expectedEscrow, pathUsd.balanceOf(address(exchange)), "pathUSD exchange balance different than expected");
     }
 
 }
